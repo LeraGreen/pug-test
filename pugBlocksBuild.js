ded0d05127d804db5016ab2964a20fee75bfedde
Nikolay Gromov <nicothin@gmail.com>
2016-06-30 20:49:28 +0300

    эксперименты

--- pugBlocksBuild.js
+++ pugBlocksBuild.js
@@ -2,35 +2,43 @@
 
 const fs = require('fs');
 const src = 'src/';
-let file;
-let fileMixins = [];
 
-let pugFiles = fs.readdirSync(src, 'utf8').filter(function(item) {
-  if(/.+\.pug$/.test(item)) return true;
-  else return false;
-});
+let mixinsList = getPugMixinsList(src);
+console.log(mixinsList);
 
-pugFiles.forEach(function(item, i) {
-  file = fs.readFileSync(src + item, 'utf8');
-  fileMixins = file.split('\n').filter(function(item) {
-    if(/[\n\s]\+/.test(item)) return true;
+
+// Получение списка примесей, использованных в файлах страниц
+function getPugMixinsList(src) {
+  let list = [];
+
+  let pugFiles = fs.readdirSync(src, 'utf8').filter(function(item) {
+    if(/.+\.pug$/.test(item)) return true;
     else return false;
   });
-  console.log(fileMixins);
-});
 
+  pugFiles.forEach(function(item) {
+    let fileMixins = fs.readFileSync(src + item, 'utf8').split('\n');
+    fileMixins.forEach(function(item) {
+      if( /[\n\s]\+/.test(item) ) {
+        list.push(item.trim().replace(/\+|\(.*\)/g, ''));
+      }
+    });
+  });
 
+  list = uniqueArray(list);
 
+  return list;
+}
 
 // Проверка существования файла
-function fileExist(path) {
-  const fs = require('fs');
-  try {
-    fs.statSync(path);
-  } catch(err) {
-    return !(err && err.code === 'ENOENT');
-  }
-}
+// function fileExist(path) {
+//   const fs = require('fs');
+//   try {
+//     fs.statSync(path);
+//   } catch(err) {
+//     return !(err && err.code === 'ENOENT');
+//   }
+// }
 
 // Оставить в массиве только уникальные значения (убрать повторы)
 function uniqueArray(arr) {
